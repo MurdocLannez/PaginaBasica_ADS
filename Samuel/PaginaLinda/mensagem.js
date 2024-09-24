9dadac784e94e43614cf683b61133fd722f4ced9
MurdocLannez <141279336+MurdocLannez@users.noreply.github.com>
2024-09-23 23:18:21 -0300

    Update mensagem.js

--- Samuel/PaginaLinda/mensagem.js
+++ Samuel/PaginaLinda/mensagem.js
@@ -1,11 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const alertButton = document.getElementById('alertButton');
+    const modal = document.getElementById('customModal');
+    const ProButton = document.getElementById('ProButton');
+    const cancelButton = document.getElementById('cancelButton');
 
     alertButton.addEventListener('click', function(event) {
         event.preventDefault();
-        alert("Bem Vindo(a) ao Site Oriental Geek!");
-        if (confirm("Deseja algo Mais? Clique em Ok")) {
-            window.location.href = "https://chatgpt.com/"; 
+        modal.style.display = 'flex'; // Mostrar o modal
+    });
+
+    ProButton.addEventListener('click', function() {
+        window.location.href = "https://chatgpt.com/";
+    });
+
+    cancelButton.addEventListener('click', function() {
+        modal.style.display = 'none'; // Fechar o modal
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
         }
     });
 });
